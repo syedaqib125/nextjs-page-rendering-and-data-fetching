092befdd8fdd2bf29e246367845aad32366b4562
Syed Aqib <syed.aqib125@gmail.com>
2022-07-11 10:01:43 +0500

    getStaticProps startup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import { Fragment } from 'react'
+import { Fragment } from "react";
+import fs from "fs/promises";
+import path from "path";
 
-export default function Home() {
+export default function Home(props) {
+  const { products } = props;
   return (
-   <Fragment>
-    <h1>Hello world!</h1>
-   </Fragment>
-  )
+    <Fragment>
+      <ul>
+        {products.map((product) => {
+          return <li key={product.id}>{product.title}</li>;
+        })}
+      </ul>
+    </Fragment>
+  );
 }
+
+export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+};
