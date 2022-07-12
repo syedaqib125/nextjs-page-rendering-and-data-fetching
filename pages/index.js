59582c62697569bfc05c7724538db9b3439d8fe4
Syed Aqib <syed.aqib125@gmail.com>
2022-07-12 11:26:33 +0500

    notFound and redirect keys

--- pages/index.js
+++ pages/index.js
@@ -14,20 +14,34 @@ export default function Home(props) {
     </Fragment>
   );
 }
-// Static Site Generation fetch all data while build prepare and rendered pre rendered data as static ,
+// SSG Static Site Generation fetch all data while build prepare and rendered pre rendered data as static ,
 // npm run dev start serve as development
 // after run npm run build cmd you can start production server on your local machine with npm start
 // recomended mostly SSG as compare to SSR
-// because SSR generate while server call but SSG pre fetched all data while buil process and shows all data as static
+// because SSR generate while server call but SSG pre fetched all data while build process and shows all data as static
 
 export const getStaticProps = async () => {
+  console.log("Re-Genertation..");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data", // if issue while conected with database we can redirect it to somewhere else route
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true }; // if data has some issue we can use notFound to show 404 error page
+  }
   return {
     props: {
       products: data.products,
     },
-    revalidate: 10, // ISR Incremental Static Generation which work as useEffect to update page after given time completed
+    revalidate: 10, // ISR Incremental Static Regeneration which work as useEffect to update page after given time completed
+    // notFound: true, //if notFound is true is returns 404 error pages
   };
 };
