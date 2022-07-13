9c88d2a0ca096c3c559619030f42fa97c1ef58f6
Syed Aqib <syed.aqib125@gmail.com>
2022-07-13 08:09:42 +0500

    fallback key

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,6 +4,10 @@ import fs from "fs/promises";
 
 const ProductDetail = (props) => {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -32,9 +36,17 @@ export const getStaticPaths = async () => {
     paths: [
       { params: { pid: "p1" } },
       { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
+      // { params: { pid: "p3" } },
     ],
-    fallback: false,
+    fallback: true,
+    //1-fallback "false" take care of only those pages whom id's are mentioned there else shows 404 page
+
+    //2-fallback "true" shows pre generated pages whom id's are mentioned there and also fetched data from id's
+    //which are not mentioned there but it will take a while, while fetching data so error handling is must here
+
+    //3-fallback "blocking" work same as true but the error handling here is excluded because when we fetching data
+    // from server , it will loading page until data is not fetched completely
   };
 };
+
 export default ProductDetail;
